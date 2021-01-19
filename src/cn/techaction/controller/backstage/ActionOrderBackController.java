a6167f2bc9a29aac1bb7b395c9053e5ff6cc3594
Liu Haoran <17611229560@163.com>
2021-01-19 15:44:03 +0800

    后台 订单列表获取

--- src/cn/techaction/controller/backstage/ActionOrderBackController.java
+++ src/cn/techaction/controller/backstage/ActionOrderBackController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.techaction.common.ResponseCode;
@@ -15,6 +17,7 @@ import cn.techaction.pojo.User;
 import cn.techaction.service.ActionOrderService;
 import cn.techaction.service.ActionUserService;
 import cn.techaction.utils.ConstUtil;
+import cn.techaction.utils.PageBean;
 import cn.techaction.vo.ActionOrderVo;
 
 @Controller
@@ -65,6 +68,31 @@ public class ActionOrderBackController {
 			return actionOrderService.mgrDetail(orderNo);
 		}
 				
+		return SverResponse.createByErrorMessage("您无操作权限!");
+	}
+	/**
+	 * 获取所有订单
+	 * @param session
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
+	@RequestMapping(value="/findorders.do",method=RequestMethod.POST)
+	@ResponseBody
+	public SverResponse<PageBean<ActionOrderVo>> getList(HttpSession session,
+			@RequestParam(value="pageNum",defaultValue="1") int pageNum,
+			@RequestParam(value="pageSize",defaultValue="10") int pageSize){
+		//判断用户是否登陆
+		User user = (User) session.getAttribute(ConstUtil.CUR_USER);
+		if(user == null) {
+			return SverResponse.createByErrorMessage("请登录后再操作!");
+		}
+		//2.用户是不是管理员
+		SverResponse<String> response=userService.isAdmin(user);
+		if(response.isSuccess()) {
+			//3.调用Service中的方法:查询订单列表
+			return actionOrderService.findOrders(user.getId(),0,pageNum,pageSize);
+		}
 		return SverResponse.createByErrorMessage("您无操作权限!");
 	}
 }
